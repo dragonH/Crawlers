89e09d84489e49680ef34cf77e0400ae753394e0
DragonH <a0261038@g2.usc.edu.tw>
2019-06-04 23:34:08 +0800

    add megabank-crawler

---
+++ megabank-crawler/README.md
@@ -0,0 +1,11 @@
+# megabank crawler
+### A crawler that crawls history exchange rate from mega bank
+## Project setup
+```
+npm install
+```
+## Usage
+```
+node index.js
+```
+
---
+++ megabank-crawler/index.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://ebank.megabank.com.tw/global2/rs/rs03/PRS3000.faces');
+  const dropDown = await page.select('#main\\:currency', '01');
+  const searchBtn = await page.click('#main\\:j_id31');
+  await page.waitForNavigation();
+  const header = await page.evaluate(() => {
+    const tds = Array.from(document.querySelectorAll('.head_td'));
+    return tds.map(td => td.innerHTML.trim());
+  });
+  const values = await page.evaluate(() => {
+    const tds = Array.from(document.querySelectorAll('.con_td'));
+    return tds.map(td => td.innerHTML.trim());
+  });
+  const result = header
+    .map((data, index) => { return { title: header[index], value: values[index]}})
+    .filter((data) => data.title !== '');
+  console.log(result);
+  await browser.close();
+})();
