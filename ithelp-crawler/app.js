6e5bec918cea7dc2e5a9486003e972973fe50abc
dragonh <a0261038@g2.usc.edu.tw>
2019-06-13 17:03:51 +0800

    Fix ithelp-crawler bug

--- ithelp-crawler/app.js
+++ ithelp-crawler/app.js
@@ -10,10 +10,12 @@ function crawlPage(pageNum) {
     await page.goto(`${baseUrl}${pageNum}`);
     const questionsList = await page.evaluate(() => {
       const questions = Array.from(document.querySelectorAll('.qa-list'));
-      return questions.map(question => {
+       return questions.map(question => {
         return {
           title: question.querySelector('.qa-list__title a').innerHTML.trim(),
-          asker: question.querySelector('.qa-list__info-link').innerHTML.trim(),
+          asker: question.querySelector('.qa-list__info-link') 
+            ? question.querySelector('.qa-list__info-link').innerHTML.trim()
+            : '匿名',
           setBestAnswer: question.querySelectorAll('.qa-condition--had-answer').length
             ? true
             : false,
@@ -26,9 +28,9 @@ function crawlPage(pageNum) {
 }
 
 async function main(pages) {
-  for (let i = 1; i <= pages; i += 1) {
+    for (let i = 357; i <= pages; i += 1) {
     const startTime = Date.now();
-    const result = await crawlPage(i);
+    const result = await crawlPage(i)
     await fs.appendFile('result.json', JSON.stringify(result), (e) => {
       if (e) {
         console.log(e);
